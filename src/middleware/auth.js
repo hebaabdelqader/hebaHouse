0ef5c615572e9ba18055a92437f84f4113dd2f72
hebaabdelqader <heba.abdelqaderr@gmail.com>
2024-05-23 11:24:21 +0300

    requirements.txt

---
+++ src/middleware/auth.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken"
+import userModel from "../../DB/models/user.model.js";
+
+export const roles={
+  Admin:'Admin',User:'User'
+}
+
+export const auth=(accessRoles=[])=>{
+    return async(req,res,next)=>{
+        const{authorization}=req.headers; 
+        if(!authorization?.startsWith(process.env.BEARERKEY))
+        return res.status(400).json({message:"invali authorization"})
+   const token=authorization.split(process.env.BEARERKEY)[1]
+   const decoded=jwt.verify(token,process.env.LOGINSECRET)
+   
+   if(!decoded)
+   return res.status(400).json({message:"invalid authorization"})
+const user= await userModel.findById(decoded.id).select("userName role")
+   if(!user)
+     return res.status(400).json({message:"not registred user"})
+   
+
+   if(!accessRoles.includes(user.role)){
+    return res.status(403).json({message:"not auth user"})
+
+   }
+  req.user=user;
+     next();
+    }
+   
+}
---
+++ src/modules/category/categories.router.js
@@ -0,0 +1,41 @@
+import {Router} from 'express'
+import * as categoriesController from './categories.controller.js';
+import fileUpload, { fileValidation } from '../../services/multer.js';
+import subCatergoryRouter from './../subcategory/subcategory.router.js'
+import { auth, roles } from '../../middleware/auth.js';
+import { endPoint } from './category.endpoint.js';
+import { asyncHandler } from "../../services/errorHandling.js";
+import * as validators from "./category.validation.js";
+import { validation } from "../../middleware/validation.js";
+const router=Router() 
+
+router.use('/:id/subcategory',subCatergoryRouter)
+router.get(
+    "/",
+    auth(endPoint.getAll),
+    asyncHandler(categoriesController.getCategories)
+  );
+  router.post(
+    "/",
+    validation(validators.createCategory),
+    auth(endPoint.create),
+    fileUpload(fileValidation.image).single("image"),
+    asyncHandler(categoriesController.createCategory)
+  );
+    router.get(
+    "/:id",
+    validation(validators.getSpecificCategory),
+    asyncHandler(categoriesController.SpecificCategory)
+  );
+  router.put(
+    "/:id",
+    auth(endPoint.update),
+    fileUpload(fileValidation.image).single("image"),
+    asyncHandler(categoriesController.updateCategory)
+  ); 
+  router.delete(
+    "/:categoryId",
+    auth(endPoint.delete),
+    asyncHandler(categoriesController.deleteCategory)
+  );
+export default router;
